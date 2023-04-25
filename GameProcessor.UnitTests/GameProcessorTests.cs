072357b20ce1f39c95b412b49bdc6e6c62e11211
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-25 13:43:40 -0500

    2 more tests added to Open Method

--- GameProcessor.UnitTests/GameProcessorTests.cs
+++ GameProcessor.UnitTests/GameProcessorTests.cs
@@ -6,8 +6,23 @@ namespace Minesweeper.UnitTests
 {
     public class GameProcessorTests
     {
+
         [Test]
-        public void T1_Open_WhenCalledWithMine_ReturnsLose()
+        public void T1_Open_WhenOpenCellIfGameStateIsNotActive_ReturnsException()
+        {
+            // Precondition
+            var boolField = new bool[2, 2];
+
+            var game = new GameProcessor(boolField);
+
+            game.Open(0, 0);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() => game.Open(1, 1));
+        }
+
+        [Test]
+        public void T2_Open_WhenCalledWithMine_ReturnsLose()
         {
             // Precondition
             var boolField = new bool[,]
@@ -24,5 +39,26 @@ namespace Minesweeper.UnitTests
             // Assert
             Assert.That(result, Is.EqualTo(GameState.Lose));
         }
+        [Test]
+        public void T3_Open_WhenAllNonMineCellsAreOpened_ReturnsWin()
+        {
+            // Precondition
+            var boolField = new bool[,]
+            {
+                { false, false},
+                { true, false}
+            };
+
+            var game = new GameProcessor(boolField);
+
+            // Action
+            var result = game.Open(1, 1);
+            result = game.Open(1,0);
+            result = game.Open(0, 0);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(GameState.Win));
+        }
+
     }
 }
