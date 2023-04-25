12adccbdd2edecf71bbbdc53d6afbb4375a7f9b2
Isaac Amortegui <isaacamortegc@outlook.com>
2023-04-25 16:07:43 -0500

    1 Negative test and repeated code commented

--- GameProcessor.UnitTests/GameProcessorTests.cs
+++ GameProcessor.UnitTests/GameProcessorTests.cs
@@ -4,11 +4,27 @@ using Minesweeper.Core.Enums;
 
 namespace Minesweeper.UnitTests
 {
+    [TestFixture]
     public class GameProcessorTests
     {
+        private bool[,] _boolField;
+        private GameProcessor _game;
 
-        [Test]
-        public void T1_Open_WhenOpenCellIfGameStateIsNotActive_ReturnsException()
+        [SetUp]
+        public void SetUp() 
+        {
+            _boolField = new bool[,]
+            {
+                { false, false},
+                { true, false}
+            };
+
+            _game = new GameProcessor(_boolField);
+        }
+
+
+/*        [Test]
+        public void T1_Open_WhenOpenCellIfGameStateIsNotActive_ReturnsException() //Negative Scenario, pending for removal.
         {
             // Precondition
             var boolField = new bool[2, 2];
@@ -19,45 +35,65 @@ namespace Minesweeper.UnitTests
 
             // Assert
             Assert.Throws<InvalidOperationException>(() => game.Open(1, 1));
-        }
-
+        }*/
+        
         [Test]
-        public void T2_Open_WhenCalledWithMine_ReturnsLose()
+        public void T1_Open_WhenCalledWithMine_ReturnsLose()
         {
             // Precondition
-            var boolField = new bool[,]
+            /*var boolField = new bool[,]
             { 
                 { false, false},
                 { false, true}
             };
 
-            var game = new GameProcessor(boolField);
+            var game = new GameProcessor(boolField);*/
 
             // Action
-            var result = game.Open(1, 1);
+           
+            var result0 = _game.Open(0, 1);
 
             // Assert
-            Assert.That(result, Is.EqualTo(GameState.Lose));
+            Assert.That(result0, Is.EqualTo(GameState.Lose));
         }
         [Test]
-        public void T3_Open_WhenAllNonMineCellsAreOpened_ReturnsWin()
+        public void T2_Open_WhenAllNonMineCellsAreOpened_ReturnsWin()
         {
-            // Precondition
+/*            // Precondition
             var boolField = new bool[,]
             {
                 { false, false},
                 { true, false}
             };
 
-            var game = new GameProcessor(boolField);
+            var game = new GameProcessor(boolField);*/
 
             // Action
-            var result = game.Open(1, 1);
-            result = game.Open(1,0);
-            result = game.Open(0, 0);
+            var result1 = _game.Open(0, 0);
+            result1 = _game.Open(1,0);
+            result1 = _game.Open(1, 1);
 
             // Assert
-            Assert.That(result, Is.EqualTo(GameState.Win));
+            Assert.That(result1, Is.EqualTo(GameState.Win));
+        }
+
+        [Test]
+        public void T3_Open_WhenCalledWithNoMine_ReturnsActive()
+        {
+            // Precondition
+/*            var boolField = new bool[,]
+            {
+                { false, false},
+                { true, false}
+            };
+
+            var game = new GameProcessor(boolField);*/
+
+            // Action
+            var result2 = _game.Open(1, 1);
+            
+            // Assert
+            Assert.That(result2, Is.EqualTo(GameState.Active));
         }
 
     }
